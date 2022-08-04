ace0ca686d8f36b21bffe1858f774d9816423458
shivam <91326162+ShivamSinghal7729@users.noreply.github.com>
2022-08-04 22:44:13 +0530

    Delete blackjack-webapp-master directory

--- blackjack-webapp-master/blackjack-angular/client/js/app/controllers/ErrorCtrl.js
+++
@@ -1,7 +0,0 @@
-angular.module('blackjack')
-.controller('ErrorCtrl', function($scope, $rootScope, $stateParams) {
-  'use strict';
-  $rootScope.pageTitle = 'Application Error Handler';
-  $scope.errorMessage = $stateParams.err;
-
-});
